75df5ebfd19d5c8d6635a329e369ee63e896a624
Xitlaly-P <jada23dem@gmail.com>
2025-08-02 22:13:05 -0500

    API Call added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,12 @@ android {
     compileSdk = 36
 
     defaultConfig {
+        val apiKey: String = if (project.hasProperty("SPOONACULAR_API_KEY")) {
+            project.property("SPOONACULAR_API_KEY") as String
+        } else {
+            ""
+        }
+        buildConfigField("String", "SPOONACULAR_API_KEY", "\"$apiKey\"")
         applicationId = "com.example.recipefinder"
         minSdk = 24
         targetSdk = 36
@@ -15,6 +21,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
     }
 
     buildTypes {
@@ -33,10 +40,15 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
-
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    // Glide v4 uses this new annotation processor -- see https://bumptech.github.io/glide/doc/generatedapi.html
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -46,4 +58,5 @@ dependencies {
 	testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.codepath.libraries:asynchttpclient:2.2.0")
 }
--- app/src/main/java/com/example/recipefinder/RecipeAdapter.kt
+++ app/src/main/java/com/example/recipefinder/RecipeAdapter.kt
@@ -2,27 +2,36 @@ package com.example.recipefinder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class RecipeAdapter (private val recipeList: List<RecipeItem>) : RecyclerView.Adapter<RecipeAdapter.ViewHolder>(){
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val recipeName: TextView = view.findViewById(R.id.nameText)
         val recipeDescription: TextView = view.findViewById(R.id.descriptionText)
         val recipeIngredients: TextView = view.findViewById(R.id.ingredientText)
+        val recipeImage: ImageView = view.findViewById(R.id.recipeImage)
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.recipe_item, parent, false)
-
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recipe_item, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = recipeList[position]
         holder.recipeName.text = item.name
-        holder.recipeDescription.text = item.description
+        holder.recipeDescription.text = item.summary
         holder.recipeIngredients.text = item.ingredients
+        if (item.imageUrl.isNotEmpty()) {
+            Glide.with(holder.recipeImage.context)
+                .load(item.imageUrl)
+                .centerCrop()
+                .into(holder.recipeImage)
+        } else {
+            holder.recipeImage.setImageResource(R.drawable.ic_launcher_foreground) // fallback
+        }
     }
 
     override fun getItemCount() = recipeList.size
